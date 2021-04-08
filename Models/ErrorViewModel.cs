ad14296e04abe5834590854762bb777833f4da81
yeon5902 <skduswo2@gmail.com>
2021-04-08 23:07:52 +0900

    github test for webapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webapp123.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
